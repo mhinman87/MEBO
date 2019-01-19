7d5d9fb4184d88d890dd2df911e50088eba30842
mhinman87 <mhinman87@gmail.com>
2019-01-19 09:57:45 -0600

    Delete app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBVmNAj8ku0uVhbfVq0cUkUNObxRuKQ_5s",
-    authDomain: "localholicdev-706f4.firebaseapp.com",
-    databaseURL: "https://localholicdev-706f4.firebaseio.com",
-    projectId: "localholicdev-706f4",
-    storageBucket: "localholicdev-706f4.appspot.com",
-    messagingSenderId: "956483534874"
-}
